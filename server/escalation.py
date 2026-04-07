09370ebcb99dc3edb56dabcc033d72c602db58cf
Matthew Foley <matthewfoley333@gmail.com>
2026-04-07 12:18:08 +1200

    add the necessary escaltion functions

--- server/escalation.py
+++ server/escalation.py
@@ -1,16 +1,19 @@
 import asyncio
 import os
 import time 
+import resend
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langfuse.client import Langfuse 
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(
   model="models/gemini-flash-latest",
-  temperature="0.7"
+  temperature="0.7",
   api_key=os.environ["GOOGLE_API_KEY"]
 )
 
@@ -20,6 +23,18 @@ langfuse = Langfuse (
     secret_key=os.environ["LANGFUSE_SECRET_KEY"],
     host=os.environ["LANGFUSE_BASE_URL"],
 )
+
+@tool
+def send_escalation_email(subject: str, body: str): 
+  #resend api key 
+  resend.api_key = os.environ["RESEND_API_KEY"]
+
+  r = resend.Emails.send(
+    "from": "AI-Assistant <onboarding@resend.dev>", # Use your verified domain in prod
+        "to": ["matthew@example.com"],
+        "subject": subject,
+        "html": f"<p>{body}</p>"
+  )
 
 def escalation_function(discord_message): 
 
